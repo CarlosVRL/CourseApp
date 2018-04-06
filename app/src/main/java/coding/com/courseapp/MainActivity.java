60007aee060cf257d43113a17139379b5400d900
CarlosVRL <carlos.varela.eng@gmail.com>
2018-04-05 23:11:55 -0500

    add simple listener

--- app/src/main/java/coding/com/courseapp/MainActivity.java
+++ app/src/main/java/coding/com/courseapp/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,7 +28,15 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                TextView textView = findViewById(R.id.text_value);
+                String stringValue = textView.getText().toString();
+                int originalValue = Integer.parseInt(stringValue);
+                int newValue = MyWorker.doubleTheValue(originalValue);
+
+                textView.setText(Integer.toString(newValue));
+
+                Snackbar.make(view, "Change value " + originalValue +
+                                    " to " + newValue, Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
